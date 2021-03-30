f999ad7e30ee6d2770295e7df55b34ffbca26d6c
haelina <56834622+haelina@users.noreply.github.com>
2021-03-30 17:46:10 +0300

    Add new HeaderComponent.

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,28 +1,112 @@
-import "./styles/HeaderComponent.css";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
-function HeaderComponent() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+    marginTop: 50,
+    marginBottom: 50,
+    marginLeft: 20,
+    textAlign: "left",
+  },
+  buttons: {},
+  button: {
+    minWidth: 150,
+    display: "block",
+    margin: 10,
+  },
+}));
+
+const HeaderComponent = ({ onChangePage }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log("Clicked menu button");
+  };
+
+  const handleClose = (nav) => {
+    setAnchorEl(null);
+    console.log("nav is" + nav);
+    onChangePage(nav);
+    //window.location.href = nav;
+  };
+
+  const clickedLogin = () => {
+    console.log("clicked login button");
+  };
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </header>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <div className={classes.title}>
+          <Typography variant="h3">CleanBuddy</Typography>
+          <Typography variant="h6">Siivouspalvelut helposti netistä</Typography>
+        </div>
+        <div className={classes.buttons}>
+          <Button
+            className={classes.button}
+            size="large"
+            variant="outlined"
+            color="inherit"
+            onClick={clickedLogin}
+          >
+            Login
+          </Button>
+          <Button
+            className={classes.button}
+            size="large"
+            variant="outlined"
+            color="inherit"
+            startIcon={<MenuIcon />}
+            onClick={handleClick}
+          >
+            Menu
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            transformOrigin={{ vertical: -65, horizontal: "center" }}
+            //aria-label="menu"
+            //aria-haspopup="true"
+          >
+            <MenuItem onClick={() => handleClose("/info")}>
+              Tietoa palvelusta
+            </MenuItem>
+            <MenuItem onClick={() => handleClose("/pikatarjoukset")}>
+              Pikatarjoukset
+            </MenuItem>
+            <MenuItem onClick={() => handleClose("/palveluntarjoajat")}>
+              Palveluntarjoajat
+            </MenuItem>
+            <MenuItem onClick={() => handleClose("/tarjouspyynto")}>
+              Pyydä tarjous
+            </MenuItem>
+            <MenuItem onClick={() => handleClose("/tietosuojaseloste")}>
+              Tietosuojaseloste
+            </MenuItem>
+          </Menu>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default HeaderComponent;
