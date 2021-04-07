df5cad9dc135ae2ce4d81ea418943f67ae8d5aaa
haelina <56834622+haelina@users.noreply.github.com>
2021-04-07 16:28:00 +0300

    Modify mainpage just a bit.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import HeaderComponent from "./HeaderComponent";
 import Company from "./Company";
+import "./styles/TextPage.css";
 
 const MainPage = () => {
   return (
     <div>
       <HeaderComponent />
       <div>
-        <h1>Sisältöä ja muuta etusivun juttua</h1>
+        <h1>Tervetuloa CleanBuddyn etusivulle!</h1>
+        <div className="TextContainer">
+          <p>Tässä meillä tulee olemaan pikatarjouksia.</p>
+        </div>
         <Company />
       </div>
     </div>
