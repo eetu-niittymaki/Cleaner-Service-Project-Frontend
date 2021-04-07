a713dcfe22c807b675e0edbaa624bd5232400841
haelina <56834622+haelina@users.noreply.github.com>
2021-04-07 17:59:12 +0300

    Add special offer with dummy data for testing.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HeaderComponent from "./HeaderComponent";
-import Company from "./Company";
 import "./styles/TextPage.css";
+import SpecialOffer from "./SpecialOffer.js";
 
 const MainPage = () => {
   return (
@@ -12,7 +12,13 @@ const MainPage = () => {
         <div className="TextContainer">
           <p>Tässä meillä tulee olemaan pikatarjouksia.</p>
         </div>
-        <Company />
+        <SpecialOffer
+          title="Perusteellinen kotisiivous"
+          duration={4}
+          description="Nullam auctor interdum nibh, ut ultricies purus suscipit non. Integer mattis mollis feugiat. Duis fringilla orci interdum, facilisis ex vitae, tempus turpis. Donec imperdiet, urna ac euismod dapibus, magna metus convallis risus, sed rhoncus diam risus at risus."
+          companyName="Siivouspojat Ky"
+          price={99}
+        />
       </div>
     </div>
   );
