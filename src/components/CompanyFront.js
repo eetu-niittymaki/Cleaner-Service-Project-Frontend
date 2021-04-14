80e8421d1d78c806ae6d1a08e535c85246f01cec
haelina <56834622+haelina@users.noreply.github.com>
2021-04-14 15:42:55 +0300

    Add company's information to front page.

--- src/components/CompanyFront.js
+++ src/components/CompanyFront.js
@@ -14,6 +14,8 @@ import Checkbox from "@material-ui/core/Checkbox";
 import React, { useState, useEffect } from "react";
 import HeaderComponent from "./HeaderComponent";
 import "./styles/TextPage.css";
+import Connection from "./BackendConnection";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -29,7 +31,51 @@ const useStyles = makeStyles((theme) => ({
 const CompanyFront = () => {
   const styles = useStyles();
 
+  const exampleData = {
+    id: 1,
+    name: "Siivouspojat Ab",
+    contactPerson: "Jussi Mäkinen",
+    phonenumber: "040 5544671",
+    address: "Mäkitie 3",
+    postnumber: "36100",
+    city: "Tampere",
+    email: "asiakaspalvelu@siivouspojat.fi",
+    description: "Tehdään loistavaa jälkeä",
+  };
+  const [company, setCompany] = useState(exampleData);
+
   const handleChange = (event) => {};
+
+  const readCompanies = async () => {
+    try {
+      const companies = Connection.getAllCompanies();
+      setCompany(companies);
+    } catch (e) {
+      alert("Problem with getting companies");
+    }
+  };
+
+  // Get companies
+  useEffect(() => {
+    //console.log("fetching company data");
+    //readCompanies();
+    /*fetch("https://clean-buddy.herokuapp.com/api/suppliers")
+      .then((res) => res.json())
+      .then((data) => setCompany(data));
+      */
+    //companies();
+  }, []);
+
+  const companies = () => {
+    if (company.length !== 0) {
+      let ui = company.map((company) => (
+        <li key={company.supplier_id}>
+          {company.name} - {company.phone} - {company.email} -{" "}
+        </li>
+      ));
+      return ui;
+    }
+  };
 
   return (
     <div>
@@ -37,7 +83,24 @@ const CompanyFront = () => {
 
       <h1>Tervetuloa yrityksen omille sivuille!</h1>
 
-      <div>Tässä on yrityksen yhteystiedot.</div>
+      <h3>Tässä on yrityksen yhteystiedot.</h3>
+      <Box border={1} mb={2} p={1}>
+        <div>Yrityksen nimi: {company.name}</div>
+        <div>Yhteyshenkilö: {company.contactPerson}</div>
+        <div>Puhelinnumero: {company.phone}</div>
+        <div>Osoite: {company.address}</div>
+        <div>Postinumero: {company.postnumber}</div>
+        <div>Postitoimipaikka: {company.city}</div>
+        <div>Sähköpostiosoite: {company.email}</div>
+        <Button
+          variant="outlined"
+          size="large"
+          color="primary"
+          //onClick={() => (window.location.href = "/")}
+        >
+          Muokkaa tietoja
+        </Button>
+      </Box>
     </div>
   );
 };
