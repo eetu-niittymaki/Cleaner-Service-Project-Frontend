606c5d02ad4f8a8325435f8e5eaf192be9463b68
haelina <56834622+haelina@users.noreply.github.com>
2021-04-21 18:20:51 +0300

    Modify component to receive suppliers from props.

--- src/components/CompaniesPage.js
+++ src/components/CompaniesPage.js
@@ -2,13 +2,27 @@ import React from "react";
 import HeaderComponent from "./HeaderComponent";
 import Company from "./Company";
 
-const CompaniesPage = () => {
+const CompaniesPage = (props) => {
+  const companyList = props.allCompanies.map((company) => {
+    return (
+      <Company
+        name={company.name}
+        address={company.streetAddress}
+        postnumber={company.postcode}
+        city={company.city}
+        phonenumber={company.phone}
+        email={company.email}
+        description={company.supplierDescription}
+      />
+    );
+  });
   return (
     <div>
       <HeaderComponent />
       <div>
         <h1>Palveluntarjoajat:</h1>
-        <Company
+        {companyList}
+        {/* <Company
           name="Yritys Oy"
           address="Mäkikatu 3"
           postnumber="33100"
@@ -34,7 +48,7 @@ const CompaniesPage = () => {
           phonenumber="040 66544734"
           email="reiska@yahoo.com"
           description="Phasellus tristique massa eget arcu mollis pellentesque. Maecenas imperdiet blandit lobortis. Aenean scelerisque in ipsum quis lacinia. Nullam auctor interdum nibh, ut ultricies purus suscipit non. Integer mattis mollis feugiat. Duis fringilla orci interdum, facilisis ex vitae, tempus turpis. Donec imperdiet, urna ac euismod dapibus, magna metus convallis risus, sed rhoncus diam risus at risus."
-        />
+        /> */}
       </div>
     </div>
   );
