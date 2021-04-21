535db4dce8428b4fce5e997ae2e716b8570d951f
haelina <56834622+haelina@users.noreply.github.com>
2021-04-21 15:24:26 +0300

    Modify component just a bit.

--- src/components/BackendConnection.js
+++ src/components/BackendConnection.js
@@ -5,6 +5,7 @@ const baseUrl = "https://clean-buddy.herokuapp.com/api/";
 const getAllCompanies = async () => {
   const result = await axios.get(`${baseUrl}suppliers/`);
   console.log("BackendConnection: Getting all companies");
+  console.log(result.data);
   return result.data;
 };
 
