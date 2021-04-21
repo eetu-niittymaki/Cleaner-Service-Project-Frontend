33ea5a945253ca51690ba22aec77a3048a0355f5
haelina <56834622+haelina@users.noreply.github.com>
2021-04-21 18:23:07 +0300

    Get all companies from backend and give them as props.

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,54 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PrivacyPolicy from "./PrivacyPolicy.js";
 import OfferRequest from "./OfferRequest";
+import BackendConnection from "./BackendConnection";
 
 const App = () => {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    const exampleData = [
+      {
+        id: 1,
+        name: "Siivouspojat Ab",
+        contactPerson: "Jussi Mäkinen",
+        phone: "040 5544671",
+        streetAddress: "Mäkitie 3",
+        postcode: "36100",
+        city: "Tampere",
+        email: "asiakaspalvelu@siivouspojat.fi",
+        supplierDescription: "Tehdään loistavaa jälkeä",
+      },
+      {
+        id: 2,
+        name: "Duunia Pukkaa Ky",
+        contactPerson: "Reiska Taipale",
+        phone: "040 5544671",
+        streetAddress: "Koivukuja 155",
+        postcode: "13340",
+        city: "Salo",
+        email: "reiskahoitaa@yahoo.com",
+        supplierDescription: "Siivotaan kun ehditään",
+      },
+    ];
+    const loadCompanyList = async () => {
+      try {
+        const companiesListed = await BackendConnection.getAllCompanies();
+        if (companiesListed.length === 0) {
+          console.log(
+            "Got empty list from backend, using example data instead"
+          );
+          setCompanies(exampleData);
+        } else {
+          setCompanies(companiesListed);
+        }
+      } catch (err) {
+        alert("Problem with loading supplier data from database");
+      }
+    };
+    loadCompanyList();
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -40,7 +86,7 @@ const App = () => {
               <InfoPage />
             </Route>
             <Route path="/companies">
-              <CompaniesPage />
+              <CompaniesPage allCompanies={companies} />
             </Route>
             <Route path="/offerRequest">
               <OfferRequest />
