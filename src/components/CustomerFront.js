22b28e7b5a50d3988106a07ca09e013de67be386
haelina <56834622+haelina@users.noreply.github.com>
2021-04-23 18:27:07 +0300

    Show customerinfo and appropriate buttons.

--- src/components/CustomerFront.js
+++ src/components/CustomerFront.js
@@ -1,43 +1,141 @@
-import TextField from "@material-ui/core/TextField";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
 import { makeStyles } from "@material-ui/core/styles";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Button from "@material-ui/core/Button";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+import Connection from "./BackendConnection";
+import { Paper, Grid, Box, Button } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import HeaderComponent from "./HeaderComponent";
 import "./styles/TextPage.css";
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    //margin: theme.spacing(1),
-    minWidth: "100%",
-    marginBottom: theme.spacing(2),
+  info: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
   },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
+  leftColumn: {
+    textAlign: "right",
+  },
+  rightColumn: {
+    textAlign: "left",
   },
 }));
 
-const CustomerFront = () => {
+const CustomerFront = ({ customerId }) => {
   const styles = useStyles();
 
-  const handleChange = (event) => {};
+  const exampleData = {
+    customer_id: 1,
+    first_name: "Pasi",
+    last_name: "Virtanen",
+    street_address: "Kotikatu 3 A",
+    city: "Tampere",
+    postcode: "33310",
+    phone: "+42312231",
+    email: "email@email.com",
+  };
+
+  const [customer, setCustomer] = useState(exampleData);
+
+  useEffect(() => {
+    // Load all companies from database and search with given props companyId
+    const loadCustomerData = async () => {
+      const temp = await Connection.getAllCustomers();
+      if (temp.length > 0) {
+        temp.filter((cust) => cust.customer_id === customerId);
+        setCustomer(temp[0]);
+      }
+    };
+    loadCustomerData();
+  }, [customerId]);
 
   return (
     <div>
       <HeaderComponent />
-
-      <h1>Tervetuloa omille sivuille!</h1>
-
-      <div></div>
+      <h1>Tervetuloa omille sivuille {customer.first_name}!</h1>
+      <Box m={1} p={2}>
+        <Grid className={styles.info} container spacing={1} p={2} mb={2}>
+          <Grid item xs={6}>
+            <Button
+              variant="outlined"
+              size="large"
+              color="primary"
+              fullWidth
+              //onClick={() => (window.location.href = "/")}
+            >
+              Ostohistoria
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button
+              variant="outlined"
+              size="large"
+              color="primary"
+              fullWidth
+              //onClick={() => (window.location.href = "/")}
+            >
+              Katso tarjouspyynnöt
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+      <h3>Tässä on asiakkaan yhteystiedot.</h3>
+      <Box border={1} m={2} p={1}>
+        <Grid className={styles.info} container spacing={1} p={2} mb={2}>
+          <Grid className={styles.leftColumn} item xs={4} sm={5}>
+            Nimi:
+          </Grid>
+          <Grid item xs={1} />
+          <Grid className={styles.rightColumn} item xs={7} sm={6}>
+            {customer.first_name} {customer.last_name}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={4} sm={5}>
+            Puhelinnumero:
+          </Grid>
+          <Grid item xs={1} />
+          <Grid className={styles.rightColumn} item xs={7} sm={6}>
+            {customer.phone}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={4} sm={5}>
+            Osoite:
+          </Grid>
+          <Grid item xs={1} />
+          <Grid className={styles.rightColumn} item xs={7} sm={6}>
+            {customer.street_address}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={4} sm={5}>
+            Postinumero:
+          </Grid>
+          <Grid item xs={1} />
+          <Grid className={styles.rightColumn} item xs={7} sm={6}>
+            {customer.postcode}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={4} sm={5}>
+            Postitoimipaikka:
+          </Grid>
+          <Grid item xs={1} />
+          <Grid className={styles.rightColumn} item xs={7} sm={6}>
+            {customer.city}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={4} sm={5}>
+            Sähköpostiosoite:
+          </Grid>
+          <Grid item xs={1} />
+          <Grid className={styles.rightColumn} item xs={7} sm={6}>
+            {customer.email}
+          </Grid>
+        </Grid>
+        <div className={styles.info}>
+          <Button
+            variant="outlined"
+            size="large"
+            color="primary"
+            //onClick={() => (window.location.href = "/")}
+          >
+            Muokkaa tietoja
+          </Button>
+        </div>
+      </Box>
+      <Box border={1} m={2} p={3}>
+        <div>Data from database is now: {JSON.stringify(customer)}</div>
+      </Box>
     </div>
   );
 };
