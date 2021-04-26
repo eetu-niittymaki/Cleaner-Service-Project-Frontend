130dcbc7be56e679e1beee85dfd5813422887605
haelina <56834622+haelina@users.noreply.github.com>
2021-04-26 10:01:55 +0300

    Add db connection and show all specialoffers.

--- src/components/AllSpecialOffers.js
+++ src/components/AllSpecialOffers.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/TextPage.css";
 import SpecialOffer from "./SpecialOffer.js";
+import BackendConnection from "./BackendConnection.js";
 
 const AllSpecialOffers = () => {
-  return (
-    <div>
-      <div>
-        <SpecialOffer
-          title="Perusteellinen kotisiivous"
-          duration={4}
-          description="Nullam auctor interdum nibh, ut ultricies purus suscipit non. Integer mattis mollis feugiat. Duis fringilla orci interdum, facilisis ex vitae, tempus turpis. Donec imperdiet, urna ac euismod dapibus, magna metus convallis risus, sed rhoncus diam risus at risus."
-          companyName="Siivouspojat Ky"
-          price={99}
-        />
-      </div>
-    </div>
-  );
+  const [specialOffers, setSpecialOffers] = useState([]);
+
+  useEffect(() => {
+    const getAllOffers = async () => {
+      try {
+        const offersListed = await BackendConnection.getAllSpecialOffers();
+        if (offersListed.length === 0) {
+          console.log(
+            "Got empty list from backend, using example data instead"
+          );
+          //setSpecialOffers(exampleData);
+        } else {
+          setSpecialOffers(offersListed);
+        }
+      } catch (err) {
+        alert("Problem with loading specialoffer data from database");
+      }
+    };
+    getAllOffers();
+  }, []);
+
+  const specialOfferList = specialOffers.map((offer) => {
+    return (
+      <SpecialOffer
+        // TODO: change key to product_id
+        key={offer.name}
+        title={offer.product_name}
+        //TODO: get real duration from db
+        duration={4}
+        companyName={offer.name}
+        price={offer.product_price}
+        description={offer.product_description}
+      />
+    );
+  });
+
+  return <div>{specialOfferList}</div>;
 };
 
 export default AllSpecialOffers;
