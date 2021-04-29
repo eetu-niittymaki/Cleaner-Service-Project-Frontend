2b6fa6c10e52fde5b8b50f3bff43a0465555d0b3
haelina <56834622+haelina@users.noreply.github.com>
2021-04-29 10:53:23 +0300

    Modify Grid items showing offreq data.

--- src/components/CompOffReqBox.js
+++ src/components/CompOffReqBox.js
@@ -6,6 +6,12 @@ const useStyles = makeStyles((theme) => ({
   backButton: {
     textAlign: "center",
   },
+  leftColumn: {
+    textAlign: "right",
+  },
+  rightColumn: {
+    textAlign: "left",
+  },
 }));
 
 const CompOffReqBox = ({ offerReq }) => {
@@ -14,25 +20,73 @@ const CompOffReqBox = ({ offerReq }) => {
 
   if (offerReq !== null) {
     return (
-      <Box m={1} p={2}>
+      <Box border={1} m={1} p={2}>
         <Grid className={styles.info} container spacing={1} p={2} mb={2}>
-          <Grid item xs={6}>
+          <Grid className={styles.leftColumn} item xs={5}>
             ID:
           </Grid>
-          <Grid item xs={6}>
+          <Grid className={styles.rightColumn} item xs={7}>
             {offerRequest.request_id}
           </Grid>
-          <Grid item xs={6}>
+          <Grid className={styles.leftColumn} item xs={5}>
             Asuntotyyppi:
           </Grid>
-          <Grid item xs={6}>
+          <Grid className={styles.rightColumn} item xs={7}>
             {offerRequest.apartment_type}
           </Grid>
-          <Grid item xs={6}>
+          <Grid className={styles.leftColumn} item xs={5}>
             Asunnon pinta-ala:
           </Grid>
-          <Grid item xs={6}>
+          <Grid className={styles.rightColumn} item xs={7}>
             {offerRequest.apartment_area}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Siivoustiheys:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.cleaning_frequency}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Asiakas:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.first_name} {offerRequest.last_name}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Osoite:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.street_address}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Postinumero:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.postcode}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Postitoimipaikka:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.city}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Puhelinnumero:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.phone}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Sähköpostiosoite:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.email}
+          </Grid>
+          <Grid className={styles.leftColumn} item xs={5}>
+            Lisätietoa:
+          </Grid>
+          <Grid className={styles.rightColumn} item xs={7}>
+            {offerRequest.optional_information}
           </Grid>
         </Grid>
       </Box>
