eee0c7dd4981b95622f5a415430b72482311b099
haelina <56834622+haelina@users.noreply.github.com>
2021-04-29 11:31:07 +0300

    Add component for showing customer related offerrequests.

---
+++ src/components/CustomerOfferRequests.js
@@ -0,0 +1,72 @@
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useEffect } from "react";
+import HeaderComponent from "./HeaderComponent";
+import BackendConnection from "./BackendConnection";
+import "./styles/TextPage.css";
+import CompOffReqBox from "./CompOffReqBox";
+
+const useStyles = makeStyles((theme) => ({
+  backButton: {
+    textAlign: "center",
+  },
+}));
+
+const CustomerOfferRequests = ({ customerEmail }) => {
+  const styles = useStyles();
+  const [offerRequests, setOfferRequests] = useState([]);
+
+  const email = "Siivouspojat";
+
+  // TODO: change this to search offers by customer email
+  const loadOfferRequests = async () => {
+    console.log("loading offer request now once");
+    let offerReqs = await BackendConnection.getOfferRequestsBySupplier(email);
+    if (offerReqs.length > 0) {
+      // Reverse result array because we want to show newest offer requests first
+      offerReqs = offerReqs.reverse();
+      setOfferRequests(offerReqs);
+    }
+  };
+
+  useEffect(() => {
+    loadOfferRequests();
+  }, []);
+
+  const showOfferList = () => {
+    return offerRequests.map((req) => {
+      return (
+        <CompOffReqBox key={req.request_id} offerReq={req} isCompany={false} />
+      );
+    });
+  };
+
+  if (offerRequests.length === 0) {
+    return (
+      <div>
+        <HeaderComponent />
+        <div>No offer requests yet.</div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <HeaderComponent />
+        <div>
+          <h1>Tässä saapuneet tarjouspyynnöt:</h1>
+          {showOfferList()}
+          <Button
+            variant="outlined"
+            size="large"
+            color="primary"
+            onClick={() => (window.location.href = "/mypage/customer")}
+          >
+            Takaisin
+          </Button>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default CustomerOfferRequests;
