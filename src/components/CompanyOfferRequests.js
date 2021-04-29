864798b08bc9a36fc7c93691430b2506d8689cfd
haelina <56834622+haelina@users.noreply.github.com>
2021-04-29 10:54:20 +0300

    Add mapping of offreqs with CompOffReq component.

--- src/components/CompanyOfferRequests.js
+++ src/components/CompanyOfferRequests.js
@@ -1,8 +1,10 @@
-import { TextField, Button, Grid } from "@material-ui/core";
+import { Button, Grid, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useState, useEffect } from "react";
 import HeaderComponent from "./HeaderComponent";
+import BackendConnection from "./BackendConnection";
 import "./styles/TextPage.css";
+import CompOffReqBox from "./CompOffReqBox";
 
 const useStyles = makeStyles((theme) => ({
   backButton: {
@@ -10,26 +12,60 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CompanyOfferRequests = () => {
+const CompanyOfferRequests = ({ supplierName }) => {
   const styles = useStyles();
+  const [offerRequests, setOfferRequests] = useState([]);
 
-  return (
-    <div>
-      <HeaderComponent />
+  const compName = "Siivouspojat";
+
+  const loadOfferRequests = async () => {
+    console.log("loading offer request now once");
+    let offerReqs = await BackendConnection.getOfferRequestsBySupplier(
+      compName
+    );
+    if (offerReqs.length > 0) {
+      // Reverse result array because we want to show newest offer requests first
+      offerReqs = offerReqs.reverse();
+      setOfferRequests(offerReqs);
+    }
+  };
+
+  useEffect(() => {
+    loadOfferRequests();
+  }, []);
+
+  const showOfferList = () => {
+    return offerRequests.map((req) => {
+      return <CompOffReqBox key={req.request_id} offerReq={req} />;
+    });
+  };
+
+  if (offerRequests.length === 0) {
+    return (
       <div>
-        <h1>Tässä saapuneet tarjouspyynnöt:</h1>
-
-        <Button
-          variant="outlined"
-          size="large"
-          color="primary"
-          onClick={() => (window.location.href = "/mypage/company")}
-        >
-          Takaisin
-        </Button>
+        <HeaderComponent />
+        <div>Loading data</div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <HeaderComponent />
+        <div>
+          <h1>Tässä saapuneet tarjouspyynnöt:</h1>
+          {showOfferList()}
+          <Button
+            variant="outlined"
+            size="large"
+            color="primary"
+            onClick={() => (window.location.href = "/mypage/company")}
+          >
+            Takaisin
+          </Button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default CompanyOfferRequests;
