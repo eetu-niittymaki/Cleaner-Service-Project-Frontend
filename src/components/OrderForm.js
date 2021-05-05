dc4c557ceb82e273a33dbaf2b4d058987edc77e5
haelina <56834622+haelina@users.noreply.github.com>
2021-05-05 14:21:40 +0300

    Fetch specialOrder data with id.

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -16,6 +16,7 @@ import React, { useState, useEffect } from "react";
 import HeaderComponent from "./HeaderComponent";
 import "./styles/TextPage.css";
 import SpecialOfferDataBox from "./SpecialOfferDataBox";
+import BackendConnection from "./BackendConnection";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -30,119 +31,138 @@ const useStyles = makeStyles((theme) => ({
 
 const OrderForm = ({ specialOfferId }) => {
   const styles = useStyles();
-  const [specialOffer, setSpecialOffer] = useState({});
+  const [specialOffer, setSpecialOffer] = useState(null);
 
   useEffect(() => {
-    console.log("apartment type or area changed");
-  }, []);
+    // Load all companies from database and search with given props companyId
+    const loadSpecialOfferData = async () => {
+      const temp = await BackendConnection.getAllSpecialOffers();
+      if (temp.length > 0) {
+        temp.filter((offer) => offer.product_id === specialOfferId);
+        setSpecialOffer(temp[0]);
+      }
+    };
+    loadSpecialOfferData();
+  }, [specialOfferId]);
 
-  return (
-    <div>
-      <HeaderComponent />
+  if (specialOffer == null) {
+    return (
       <div>
-        <h1>Tilauksen tiedot:</h1>
-        <div className="TextContainer">
-          <form
-            style={{ textAlign: "left", marginBottom: 30 }}
-            autoComplete="false"
-          >
-            <h3>Siivoustarjouksen tiedot:</h3>
-            <SpecialOfferDataBox specialOffer={specialOffer} />
-          </form>
-          <form style={{ textAlign: "left" }}>
-            <h3>Asiakkaan tiedot:</h3>
-            <div>
-              <TextField
-                className={styles.formControl}
-                required
-                id="standard-required"
-                label="Etunimi"
-                placeholder="Etunimi"
-                variant="outlined"
-              />
-              <TextField
-                className={styles.formControl}
-                required
-                id="standard-required"
-                label="Sukunimi"
-                placeholder="Sukunimi"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                className={styles.formControl}
-                required
-                id="standard-required"
-                label="Osoite"
-                placeholder="Osoite"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                className={styles.formControl}
-                required
-                id="standard-required"
-                label="Postinumero"
-                placeholder="Postinumero"
-                variant="outlined"
-              />
-              <TextField
-                className={styles.formControl}
-                required
-                id="standard-required"
-                label="Postitoimipaikka"
-                placeholder="Postitoimipaikka"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                className={styles.formControl}
-                id="standard-required"
-                label="Puhelinnumero"
-                placeholder="Puhelinnumero"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                className={styles.formControl}
-                required
-                id="standard-required"
-                type="email"
-                label="Sähköpostiosoite"
-                placeholder="Sähköpostiosoite"
-                variant="outlined"
-              />
-            </div>
-            <TextField
-              className={styles.formControl}
-              id="standard-required"
-              label="Lisätietoa"
-              fullWidth
-              multiline
-              rows={4}
-              rowsMax={7}
-              placeholder="Tähän voit antaa lisätietoa, esimerkiksi onko asunnossa lemmikkejä."
-              variant="outlined"
-            />
-          </form>
+        <HeaderComponent />
+        <div>
+          <h1>Kyseistä pikatarjousta ei löytynyt!</h1>
         </div>
       </div>
-      <div className="bottomButtons">
-        <Button
-          variant="outlined"
-          size="large"
-          color="primary"
-          onClick={() => (window.location.href = "/")}
-        >
-          Vahvista tilaus
-        </Button>
+    );
+  } else {
+    return (
+      <div>
+        <HeaderComponent />
+        <div>
+          <h1>Tilauksen tiedot:</h1>
+          <div className="TextContainer">
+            <form
+              style={{ textAlign: "left", marginBottom: 30 }}
+              autoComplete="false"
+            >
+              <h3>Siivoustarjouksen tiedot:</h3>
+              <SpecialOfferDataBox specialOffer={specialOffer} />
+            </form>
+            <form style={{ textAlign: "left" }}>
+              <h3>Asiakkaan tiedot:</h3>
+              <div>
+                <TextField
+                  className={styles.formControl}
+                  required
+                  id="standard-required"
+                  label="Etunimi"
+                  placeholder="Etunimi"
+                  variant="outlined"
+                />
+                <TextField
+                  className={styles.formControl}
+                  required
+                  id="standard-required"
+                  label="Sukunimi"
+                  placeholder="Sukunimi"
+                  variant="outlined"
+                />
+              </div>
+              <div>
+                <TextField
+                  className={styles.formControl}
+                  required
+                  id="standard-required"
+                  label="Osoite"
+                  placeholder="Osoite"
+                  variant="outlined"
+                />
+              </div>
+              <div>
+                <TextField
+                  className={styles.formControl}
+                  required
+                  id="standard-required"
+                  label="Postinumero"
+                  placeholder="Postinumero"
+                  variant="outlined"
+                />
+                <TextField
+                  className={styles.formControl}
+                  required
+                  id="standard-required"
+                  label="Postitoimipaikka"
+                  placeholder="Postitoimipaikka"
+                  variant="outlined"
+                />
+              </div>
+              <div>
+                <TextField
+                  className={styles.formControl}
+                  id="standard-required"
+                  label="Puhelinnumero"
+                  placeholder="Puhelinnumero"
+                  variant="outlined"
+                />
+              </div>
+              <div>
+                <TextField
+                  className={styles.formControl}
+                  required
+                  id="standard-required"
+                  type="email"
+                  label="Sähköpostiosoite"
+                  placeholder="Sähköpostiosoite"
+                  variant="outlined"
+                />
+              </div>
+              <TextField
+                className={styles.formControl}
+                id="standard-required"
+                label="Lisätietoa"
+                fullWidth
+                multiline
+                rows={4}
+                rowsMax={7}
+                placeholder="Tähän voit antaa lisätietoa, esimerkiksi onko asunnossa lemmikkejä."
+                variant="outlined"
+              />
+            </form>
+          </div>
+        </div>
+        <div className="bottomButtons">
+          <Button
+            variant="outlined"
+            size="large"
+            color="primary"
+            onClick={() => (window.location.href = "/")}
+          >
+            Vahvista tilaus
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default OrderForm;
