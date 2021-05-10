8ba3596e1c00a8dccccba08f2fda05ab131250fe
haelina <56834622+haelina@users.noreply.github.com>
2021-05-10 16:40:49 +0300

    Add purple custom buttons.

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -7,15 +7,13 @@ import AdminModifyCompanyData from "./AdminModifyCompanyData";
 import AdminModifyCustomerData from "./AdminModifyCustomerData";
 import AdminModifyOfferData from "./AdminModifyOfferData";
 import AddSupplier from "./AddSupplier";
+import { PurpleButton } from "./CustomButtons";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     //margin: theme.spacing(1),
     minWidth: "100%",
     marginBottom: theme.spacing(2),
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
   },
 }));
 
@@ -38,7 +36,7 @@ const AdminPage = () => {
     const checkIfAdmin = async () => {
       //TODO
       //check from backend if current user has admin rights
-      const getAdmin = localStorage.getItem("admin")
+      const getAdmin = localStorage.getItem("admin");
       if (getAdmin) {
         setAdminRights(true);
       }
@@ -64,32 +62,32 @@ const AdminPage = () => {
 
   const updateCustomers = async () => {
     await BackendConnection.getAllCustomers();
-  }
+  };
   const updateCompanies = async () => {
     await BackendConnection.getAllCompanies();
-  }
+  };
   const updateOffers = async () => {
     await BackendConnection.getAllSpecialOffers();
-  }
+  };
 
   const deleteCustomer = async (id) => {
-    await BackendConnection.deleteCustomer(id);//.then;
+    await BackendConnection.deleteCustomer(id); //.then;
     //setSelectedPage([]);
     //setSelectedPage(customersTxt);
   };
   const deleteCompany = async (id) => {
-    await BackendConnection.deleteSupplier(id);//.then;
+    await BackendConnection.deleteSupplier(id); //.then;
     //setSelectedPage([]);
     //setSelectedPage(companiesTxt);
   };
   const deleteOffer = async (id) => {
-    await BackendConnection.deleteOffer(id);//.then;
+    await BackendConnection.deleteOffer(id); //.then;
     //setSelectedPage([]);
     //setSelectedPage(offersTxt);
   };
 
   const getContent = (selectedP) => {
-    if (selectedP == customersTxt) {
+    if (selectedP === customersTxt) {
       return (
         <div>
           <h1>Muokkaa asiakastietoja:</h1>
@@ -97,15 +95,15 @@ const AdminPage = () => {
           {customers.map((data) => (
             <ul key={data.customer_id}>
               <AdminModifyCustomerData
-                cData = {data}
-                update = {() => updateCustomers()}
-                cDelete = {() => deleteCustomer(data.customer_id)}
+                cData={data}
+                update={() => updateCustomers()}
+                cDelete={() => deleteCustomer(data.customer_id)}
               />
             </ul>
           ))}
         </div>
       );
-    } else if (selectedPage == companiesTxt) {
+    } else if (selectedPage === companiesTxt) {
       return (
         <div>
           <h1>Muokkaa yritystietoja:</h1>
@@ -113,15 +111,15 @@ const AdminPage = () => {
           {companies.map((data) => (
             <ul key={data.supplier_id}>
               <AdminModifyCompanyData
-                cData = {data}
-                update = {() => updateCompanies()}
-                cDelete = {() => deleteCompany(data.supplier_id)}
+                cData={data}
+                update={() => updateCompanies()}
+                cDelete={() => deleteCompany(data.supplier_id)}
               />
             </ul>
           ))}
         </div>
       );
-    } else if (selectedPage == offersTxt) {
+    } else if (selectedPage === offersTxt) {
       //product_id
       return (
         <div>
@@ -130,15 +128,15 @@ const AdminPage = () => {
           {offers.map((data) => (
             <ul key={data.product_id}>
               <AdminModifyOfferData
-                oData = {data}
-                update = {() => updateOffers()}
-                oDelete = {() => deleteOffer(data.product_id)}
+                oData={data}
+                update={() => updateOffers()}
+                oDelete={() => deleteOffer(data.product_id)}
               />
             </ul>
           ))}
         </div>
       );
-    } else if (selectedPage == supplierTxt) {
+    } else if (selectedPage === supplierTxt) {
       return (
         <div>
           <AddSupplier />
@@ -146,7 +144,7 @@ const AdminPage = () => {
       );
     }
     // works as a pageswitcher -> reload data (useEffect)
-      // or maybe it doesnt, idk, everythings a mess
+    // or maybe it doesnt, idk, everythings a mess
     else {
       return (
         <div>
@@ -162,38 +160,38 @@ const AdminPage = () => {
         <div>
           <div>
             <br />
-            <Button
+            <PurpleButton
               variant="contained"
               size="large"
-              color={selectedPage == supplierTxt ? "primary" : "default"}
+              color={selectedPage === supplierTxt ? "primary" : "default"}
               onClick={() => setSelectedPage(supplierTxt)}
             >
               {supplierTxt}
-            </Button>
-            <Button
+            </PurpleButton>
+            <PurpleButton
               variant="contained"
               size="large"
-              color={selectedPage == customersTxt ? "primary" : "default"}
+              color={selectedPage === customersTxt ? "primary" : "default"}
               onClick={() => setSelectedPage(customersTxt)}
             >
               {customersTxt}
-            </Button>
-            <Button
+            </PurpleButton>
+            <PurpleButton
               variant="contained"
               size="large"
-              color={selectedPage == companiesTxt ? "primary" : "default"}
+              color={selectedPage === companiesTxt ? "primary" : "default"}
               onClick={() => setSelectedPage(companiesTxt)}
             >
               {companiesTxt}
-            </Button>
-            <Button
+            </PurpleButton>
+            <PurpleButton
               variant="contained"
               size="large"
-              color={selectedPage == offersTxt ? "primary" : "default"}
+              color={selectedPage === offersTxt ? "primary" : "default"}
               onClick={() => setSelectedPage(offersTxt)}
             >
               {offersTxt}
-            </Button>
+            </PurpleButton>
             <br />
             {getContent(selectedPage)}
           </div>
