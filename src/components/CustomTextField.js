4a36f66b5f0da2ede56ce304659fcbe8e1ac4441
haelina <56834622+haelina@users.noreply.github.com>
2021-05-10 19:04:43 +0300

    Add customized textField.

---
+++ src/components/CustomTextField.js
@@ -0,0 +1,25 @@
+import { TextField } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+export const CustomTextField = withStyles({
+  root: {
+    // This changes the label colour
+    "& label.Mui-focused": {
+      color: "#A6038D",
+    },
+    // "& .MuiInput-underline:after": {
+    //   borderBottomColor: "green",
+    // },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "gray",
+      },
+      "&:hover fieldset": {
+        borderColor: "gray",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "gray",
+      },
+    },
+  },
+})(TextField);
