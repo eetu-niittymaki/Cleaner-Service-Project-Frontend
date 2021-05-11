43bd82c794d5876e17a81931ee9ed369032a1974
haelina <56834622+haelina@users.noreply.github.com>
2021-05-11 09:20:58 +0300

    Fix component to fetch specialoffers with companyId.

--- src/components/MySpecialOffers.js
+++ src/components/MySpecialOffers.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
 import BackendConnection from "./BackendConnection";
 import CompanySpecialOffer from "./CompanySpecialOffer";
 import { PurpleButton } from "./CustomButtons";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    marginTop: 10,
+    marginBottom: 30,
+  },
+}));
 
 const MySpecialOffers = ({ companyId }) => {
+  const styles = useStyles();
   const [specialOffers, setSpecialOffers] = useState([]);
 
   const loadSpecialOffers = async () => {
     console.log("loading all special offers now once");
     let specOffers = await BackendConnection.getAllSpecialOffers();
     if (specOffers.length > 0) {
-      // TODO: change this to filter values with companyId props
-      let temp = specOffers.filter((spec) => spec.supplier_id === 2);
+      let temp = specOffers.filter((spec) => spec.supplier_id === companyId);
       if (temp.length > 0) {
         temp = temp.reverse();
         setSpecialOffers(temp);
@@ -35,6 +42,7 @@ const MySpecialOffers = ({ companyId }) => {
       <div>
         <h3>Et ole luonut vielä pikatarjouksia.</h3>
         <PurpleButton
+          className={styles.button}
           variant="outlined"
           size="large"
           color="primary"
@@ -50,6 +58,7 @@ const MySpecialOffers = ({ companyId }) => {
         <h3>Omat pikatarjoukset:</h3>
         {showSpecialOfferList()}
         <PurpleButton
+          className={styles.button}
           variant="outlined"
           size="large"
           color="primary"
