cb29c48e0a5471a8e9fb142d46dd4a9cd9040478
haelina <56834622+haelina@users.noreply.github.com>
2021-05-11 10:52:42 +0300

    Remove unused code.

--- src/components/CompaniesPage.js
+++ src/components/CompaniesPage.js
@@ -34,10 +34,10 @@ const CompaniesPage = (props) => {
       try {
         const companiesListed = await BackendConnection.getAllCompanies();
         if (companiesListed.length === 0) {
-          console.log(
+          /*console.log(
             "Got empty list from backend, using example data instead"
           );
-          //setCompanies(exampleData);
+          //setCompanies(exampleData); */
         } else {
           setCompanies(companiesListed);
         }
@@ -49,7 +49,7 @@ const CompaniesPage = (props) => {
     };
     loadCompanyList();
   }, []);
-  const companyList = /*props.allCompanies.*/ companies.map((company) => {
+  const companyList = companies.map((company) => {
     return (
       <Company
         key={company.name}
@@ -67,15 +67,7 @@ const CompaniesPage = (props) => {
     <div>
       <h1>Palveluntarjoajat:</h1>
       {companyList}
-      {/* <Company
-          name="Yritys Oy"
-          address="Mäkikatu 3"
-          postnumber="33100"
-          city="Tampere"
-          phonenumber="040 81544734"
-          email="info@yritys.com"
-          description="Phasellus tristique massa eget arcu mollis pellentesque. Maecenas imperdiet blandit lobortis. Aenean scelerisque in ipsum quis lacinia. Nullam auctor interdum nibh, ut ultricies purus suscipit non. Integer mattis mollis feugiat. Duis fringilla orci interdum, facilisis ex vitae, tempus turpis. Donec imperdiet, urna ac euismod dapibus, magna metus convallis risus, sed rhoncus diam risus at risus."
-        />
+      {/*
         <Company
           name="Siivouspojat Ab"
           address="Liinakatu 33"
