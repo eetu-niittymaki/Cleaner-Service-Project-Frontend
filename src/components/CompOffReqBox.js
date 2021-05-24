3089569c2843f249cd33e94eef97aa724beb3db1
haelina <56834622+haelina@users.noreply.github.com>
2021-05-24 10:42:11 +0300

    Remove unused import.

--- src/components/CompOffReqBox.js
+++ src/components/CompOffReqBox.js
@@ -1,6 +1,6 @@
 import { Grid, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
   backButton: {
